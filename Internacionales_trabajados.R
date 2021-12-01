851e2bc108e2e826a0e83013d644f1d82e90acb2
rocioalonsolh <rocy1115@gmail.com>
2021-12-01 11:38:07 +0100

    Añadido análisis por nacionalidad

--- Internacionales_trabajados.R
+++ Internacionales_trabajados.R
@@ -20,6 +20,14 @@ Internacionales_sexo<-
 
 View(Internacionales_sexo)
 
+#Hacemos comparativas de muertes por nacionalidad
+Internacionales_nacionalidad<-
+  Internacionales_2019 %>%
+  group_by(Nacionalidad)%>%
+  summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE)))  
+
+View(Internacionales_nacionalidad)
+
 #Unimos por variables de 2 en 2
 
 Internacionales_Causas_Sexo<-
@@ -29,3 +37,23 @@ Internacionales_Causas_Sexo<-
 
 View(Internacionales_Causas_Sexo)
 
+Internacionales_Nacionalidad_Sexo<-
+  Internacionales_2019 %>%
+  group_by(Nacionalidad, Sexo)%>%
+  summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE)))  
+
+View(Internacionales_Nacionalidad_Sexo)
+
+Internacionales_Nacionalidad_Sexo<-
+  Internacionales_2019 %>%
+  group_by(Nacionalidad, Sexo)%>%
+  summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE)))  
+
+View(Internacionales_Nacionalidad_Sexo)
+
+Internacionales_Nacionalidad_Causa<-
+  Internacionales_2019 %>%
+  group_by(Nacionalidad, Causas)%>%
+  summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE)))  
+
+View(Internacionales_Nacionalidad_Causa)
