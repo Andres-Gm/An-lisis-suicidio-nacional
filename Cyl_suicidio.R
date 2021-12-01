db2f569995b090a7ce4ea25338f6667d38a50b72
rocioalonsolh <rocy1115@gmail.com>
2021-12-01 11:31:07 +0100

    Datos de castilla y león suicidios

---
+++ Cyl_suicidio.R
@@ -0,0 +1,53 @@
+#Filtramos para tener sólo los datos referentes al suicidio,
+#es similar al código trabajado hasta ahora pero nos 
+#permite centrarnos en nuestro objetivo
+
+Cyl_suicidio<-filter(Cyl_2019, (Causas=="098 Suicidio y lesiones autoinfligidas"))
+
+View(Cyl_suicidio)
+
+#Hacemos comparativas de muertes por provincias
+Cyl_provincia<-
+  Cyl_suicidio %>%
+  group_by(Provincias)%>%
+  summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE)))  
+
+View(Cyl_provincia)
+
+#Hacemos comparativas de muertes por edades
+Cyl_edad<-
+  Cyl_suicidio %>%
+  group_by(Edad)%>%
+  summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE)))  
+
+View(Cyl_edad)
+
+#Hacemos comparativas de muertes por sexos
+Cyl_sexo<-
+  Cyl_suicidio %>%
+  group_by(Sexo)%>%
+  summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE)))  
+
+View(Cyl_sexo)
+
+#Unimos por variables de 2 en 2
+Cyl_Provincias_Edades<-
+  Cyl_suicidio %>%
+  group_by(Provincias, Edad)%>%
+  summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE)))  
+
+View(Cyl_Provincias_Edades)
+
+Cyl_Provincias_Sexo<-
+  Cyl_suicidio %>%
+  group_by(Provincias, Sexo)%>%
+  summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE)))  
+
+View(Cyl_Provincias_Sexo)
+
+Cyl_Sexo_Edades<-
+  Cyl_suicidio %>%
+  group_by(Sexo, Edad)%>%
+  summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE)))  
+
+View(Cyl_Sexo_Edades)
