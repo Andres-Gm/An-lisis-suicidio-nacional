4da8e0188238c570c187f474e879ef9143763283
rocioalonsolh <rocy1115@gmail.com>
2021-12-01 12:25:40 +0100

    Arreglo de errores

--- Nacionales_trabajados.R
+++ Nacionales_trabajados.R
@@ -2,7 +2,7 @@
 #Reducimos a la columna de muertes para trabajar desde ella
 #Renombramos las columnas para que sea mas comodo trabajar con ellas
 names (Nacionales_historico) = c("Causas", "Sexo", "Edad", "Periodo", "Total")
-Nacionales_historico_filtro<-filter(Nacionales_historico, ("Periodo"<2005))
+Nacionales_historico_filtro<-filter(Nacionales_historico, (Periodo>2005))
 
 #Dividimos por causas para encontrar las sumas de cada causa y compararlas con las tasas de suicidio
 Nacionales_causas<-
@@ -16,7 +16,6 @@ View(Nacionales_causas)
 Nacionales_edad<-
   Nacionales_historico_filtro %>%
   group_by(Edad)%>%
-  summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE)))  
 
 View(Nacionales_edad)
 
@@ -24,7 +23,6 @@ View(Nacionales_edad)
 Nacionales_sexo<-
   Nacionales_historico_filtro %>%
   group_by(Sexo)%>%
-  summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE)))  
 
 View(Nacionales_sexo)
 
@@ -32,41 +30,33 @@ View(Nacionales_sexo)
 Nacionales_Causas_Edades<-
   Nacionales_historico_filtro %>%
   group_by(Causas, Edad)%>%
-  summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE)))  
 
 View(Nacionales_Causas_Edades)
 
 Nacionales_Causas_Sexo<-
   Nacionales_historico_filtro %>%
-  group_by(Causas, Sexo)%>%
-  summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE)))  
-
+  group_by(Causas, Sexo)
 View(Nacionales_Causas_Sexo)
 
 Nacionales_Sexo_Edades<-
   Nacionales_historico_filtro %>%
-  group_by(Sexo, Edad)%>%
-  summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE)))  
+  group_by(Sexo, Edad)
 
 View(Nacionales_Sexo_Edades)
 
 Nacionales_Causa_Periodo<-
   Nacionales_historico_filtro %>%
-  group_by(Causas, Periodo)%>%
-  summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE)))  
-
+  group_by(Causas, Periodo)
+ 
 View(Nacionales_Causa_Periodo)
 
 Nacionales_Edad_Periodo<-
   Nacionales_historico_filtro %>%
-  group_by(Edad, Periodo)%>%
-  summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE)))  
+  group_by(Edad, Periodo)
 
 View(Nacionales_Edad_Periodo)
 
 Nacionales_Sexo_Periodo<-
   Nacionales_historico_filtro %>%
-  group_by(Causas, Periodo)%>%
-  summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE)))  
-
+  group_by(Causas, Periodo)
 View(Nacionales_Sexo_Periodo)
