b41892cfc6df7166ab2b46091515b0314345debd
Andres-Gm <80346445+Andres-Gm@users.noreply.github.com>
2021-12-12 22:29:12 +0100

    Corrección de las gráficas de CyL y de nacional

--- Cyl_suicidio.R
+++ Cyl_suicidio.R
@@ -74,11 +74,25 @@ Cyl_Sexo_Edades<-
   filter(., Edad != "Todas las edades")%>%
   filter(., Edad != "Menores de 1 año")
 
+
+Cyl_Sexo_Edades$Edad[ Cyl_Sexo_Edades$Edad == "De 1 a 14 años"] <- "1 - 14"
+Cyl_Sexo_Edades$Edad[ Cyl_Sexo_Edades$Edad == "De 15 a 29 años"] <- "15 - 29"
+Cyl_Sexo_Edades$Edad[ Cyl_Sexo_Edades$Edad == "De 30 a 39 años"] <- "30 - 39"
+Cyl_Sexo_Edades$Edad[ Cyl_Sexo_Edades$Edad == "De 40 a 44 años"] <- "40 - 44"
+Cyl_Sexo_Edades$Edad[ Cyl_Sexo_Edades$Edad == "De 45 a 49 años"] <- "45 - 49"
+Cyl_Sexo_Edades$Edad[ Cyl_Sexo_Edades$Edad == "De 50 a 54 años"] <- "50 - 54"
+Cyl_Sexo_Edades$Edad[ Cyl_Sexo_Edades$Edad == "De 55 a 59 años"] <- "55 - 59"
+Cyl_Sexo_Edades$Edad[ Cyl_Sexo_Edades$Edad == "De 60 a 64 años"] <- "60 - 64"
+Cyl_Sexo_Edades$Edad[ Cyl_Sexo_Edades$Edad == "De 65 a 69 años"] <- "65 - 69"
+Cyl_Sexo_Edades$Edad[ Cyl_Sexo_Edades$Edad == "De 70 a 74 años"] <- "70 - 74"
+Cyl_Sexo_Edades$Edad[ Cyl_Sexo_Edades$Edad == "De 75 a 79 años"] <- "75 - 79"
+Cyl_Sexo_Edades$Edad[ Cyl_Sexo_Edades$Edad == "De 80 a 84 años"] <- "80 - 84"
+Cyl_Sexo_Edades$Edad[ Cyl_Sexo_Edades$Edad == "De 85 a 89 años"] <- "85 - 89"
+Cyl_Sexo_Edades$Edad[ Cyl_Sexo_Edades$Edad == "De 90 a 94 años"] <- "90 - 94"
+Cyl_Sexo_Edades$Edad[ Cyl_Sexo_Edades$Edad == "De 95 años y más"] <- "95 o más"
+
 ggplot(data = Cyl_Sexo_Edades, aes(x = Edad, y = Total, fill = Sexo)) +
   geom_col(position = "dodge")
 
   
 View(Cyl_Sexo_Edades)
-
-
-
